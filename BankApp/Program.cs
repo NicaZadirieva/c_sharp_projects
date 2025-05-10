83d65598a8e8c8f153e8c5d9bd4caee2f57f3058
Veronica <no.demchenko@yandex.ru>
2025-05-10 19:00:52 +0300

    feat: добавила примеры

--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-new Account().withdraw(5);
+﻿new Account(150).withdraw(5);
+
+// недостаточно средств
+// new CreditAccount(0, 100).withdraw(5);
+var currentTime = DateOnly.FromDateTime(DateTime.Now);
+
+// AccountError: Запрещено снимать деньги до установленной даты: 11 мая 2025 г.
+new SavingsAccount(100, currentTime.AddDays(1)).withdraw(5);
