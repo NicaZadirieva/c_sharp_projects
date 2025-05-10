e1f1ca1ee49b9bc669267ac50b0f2fcbfbad7e74
Veronica <no.demchenko@yandex.ru>
2025-05-10 18:55:01 +0300

    feat: добавила кредитный счет

--- BankApp/app/model/CreditAccount/CreditAccount.cs
+++ BankApp/app/model/CreditAccount/CreditAccount.cs
@@ -1,12 +1,37 @@
+using System.Reflection;
+
 class CreditAccount : IAccountActions
 {
-    public void deposit(float amount)
+    // денег на счету
+    private float amount;
+
+    // максимум за одно снятие
+    private float maxValueAtWithdraw;
+
+    public CreditAccount(float amount, float maxValueAtWithdraw)
     {
-        throw new NotImplementedException();
+        this.amount = amount;
+        this.maxValueAtWithdraw = maxValueAtWithdraw;
     }
 
     public void withdraw(float amount)
     {
-        throw new NotImplementedException();
+        if (amount > this.maxValueAtWithdraw)
+        {
+            throw new AccountError("Превышено значение для списания");
+        }
+        else if (this.amount - amount < 0)
+        {
+            throw new AccountError("Недостаточно средств для списания платежа");
+        }
+        else
+        {
+            this.amount -= amount;
+        }
+    }
+
+    public void deposit(float amount)
+    {
+        this.amount += amount;
     }
 }
